26746f6e5f0334fd490484ef1c20ca9d19548f6e
vipgost2095-dotcom <vipgost2095@gmail.com>
2026-08-08 09:13:32 +0300

    Create main.tsx

---
+++ frontend/src/main.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { TonConnectUIProvider } from '@tonconnect/ui-react';
+import App from './App';
+import './i18n';
+import './index.css';
+
+// Инициализация Telegram WebApp
+if (window.Telegram?.WebApp) {
+  window.Telegram.WebApp.expand();
+  window.Telegram.WebApp.ready();
+}
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <TonConnectUIProvider manifestUrl="/tonconnect-manifest.json">
+      <App />
+    </TonConnectUIProvider>
+  </React.StrictMode>
+);
